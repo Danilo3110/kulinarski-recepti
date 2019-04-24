695e4fe533264a866ba572fa86fe47602fafe671
Danilo3110 <lukic3@sbb.rs>
2019-04-24 14:28:14 +0200

    add rendering for small recipes cards and css for cards on home page, updated detabase for testing

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,43 @@ async function getBase(location) {
     return responsFromBase.data;
 };
 
+async function renderRecipes() {
+    const recipes = await getBase('/recipes?_sort=id&_order=desc');
+    const limitRecipes = recipes.slice(0, 8);
+    (async () => await _render_one_recipe(limitRecipes, '.recipes-container'))();
+};
+
+async function renderAllRecipes() {
+    $('.content').html(`<h1 class="recipes-click-scroll">Vrhunski recepti - baza recepata:</h1><div class="recipes-container"></div>`);
+    const recipesAll = await getBase('/recipes');
+    (async () => await _render_one_recipe(recipesAll, '.recipes-container'))();
+    animateFocus('.recipes-click-scroll');
+};
+
+async function _render_one_recipe(recipes, location) {
+    for (const rec of recipes) {
+        const $recipeContainer = $(`${location}`);
+        const $recipe = $(`<div class="recipes" id="${rec.id}">
+                    <div class="recipes-descr">
+                        <h3>Datum objave: ${(rec.recipeCreated).slice(0, 10)}<i class="fas fa-share-alt fa-lg"></i>
+                        <span id="fav_${rec.id}"><i title="Dodaj u omiljene" class="far fa-heart fa-lg offHeart"></i></span>
+                        </h3>
+                    </div>
+                    <img src="${rec.imgUrl[0] == undefined ? './img/image-not-found.jpg' : rec.imgUrl[0]}" alt="recept" class="image_${rec.id}"><br>
+                    <div class="recipe-info">
+                        <h3 class="recipes-ctgr">${rec.category}</h3>
+                        <h2 class="recipes-descr" id="recipes-height">${rec.title}</h2><br>
+                        <hr>
+                        <h3 class="recipes-descr"><i class="fas fa-stopwatch fa-lg"></i>&nbsp;${rec.timePrep}&nbsp;&nbsp;&nbsp;
+                        <i class="fas fa-utensils fa-lg"></i>&nbsp;${rec.preparation}</h3>
+                    </div>
+                </div>`);
+        $recipe.appendTo($recipeContainer);
+    }
+};
+
 function animateFocus(toLocation) {
-    $('html, body').animate({
-        scrollTop: $(`${toLocation}`).offset().top}, 800);
+    $('html, body').animate({ scrollTop: $(`${toLocation}`).offset().top }, 800);
 };
 
 function advancedSearch() {
@@ -23,9 +57,7 @@ function advancedSearch() {
 
 function animationsAll() {
     $(window).scroll(function () {
-        $('.container').css({
-            "background-position": "0% " + ($(this).scrollTop() / 50) + "px"
-        });
+        $('.container').css({ "background-position": "0% " + ($(this).scrollTop() / 50) + "px" });
     });
     $(document).ready(() => {
         $(window).scroll(() => {
@@ -36,9 +68,7 @@ function animationsAll() {
     });
     $(document).ready(() => {
         $('.recipes-click-scroll').on('click', () => {
-            $('html, body').animate({
-                scrollTop: $('.recipes-click-scroll').offset().top
-            }, 850);
+            $('html, body').animate({ scrollTop: $('.recipes-click-scroll').offset().top }, 850);
         });
     });
 };
@@ -85,11 +115,19 @@ function logInOut() {
     }
 };
 
+function onLoadPageHTML() {
+    const page = location.href;
+    if (page.search('/index.html') >= 0) {
+        return renderRecipes();
+    }
+};
+
 function eventsAll() {
+    $('#recipes_showAll').on('click', renderAllRecipes);
     $('#aSearch, #closeSearch').on('click', advancedSearch);
     $('#logIn').on('click', userLogIn);
     $('#logIn-out').on('click', logInOut);
     $('#home').on('click', () => animateFocus('#home'));
 };
 
-$(document).on('load', addLogOut(), eventsAll(), animationsAll());
+$(document).on('load', onLoadPageHTML(), addLogOut(), eventsAll(), animationsAll());
