8ee70836d55bb1bf831b5c16abd974a67a63809b
Danilo3110 <lukic3@sbb.rs>
2019-05-01 02:06:32 +0200

    added RegEx testing for user registration, and small changes in code

--- src/js/main.js
+++ src/js/main.js
@@ -260,17 +260,61 @@ async function postIntoDatabase(location, obj, message) {
         });
 };
 
-function createUser() {
-    const usersObj = {};
-    $("#writeRecipe").find("input, select, textarea").each(function () {
-        usersObj[this.name] = $(this).val();
-    });
-    delete usersObj.passwordRepeat;
-    usersObj['favorites'] = [];
+const valid = {};
+function validationCheck() {
+    let input = event.currentTarget.name;
+    const RegEx = {
+        name: /^[A-ZŠĐŽČĆ][a-zšđžčć]{2,}\s[A-ZŠĐŽČĆ][a-zšđžčć]+$/,
+        email: /^[a-z][a-z.-\d]+[@][a-z]{3,}.[a-z]{2,3}$/,
+        password: /^(?=.*\w).{8,}$/,
+        work: /^[A-ZŠĐŽČĆ][a-zšđžčć]{2,}\s|([A-ZŠĐŽČĆ]|[a-zšđžčć]+)/,
+        telephone: /^\d{3}\/(\d{2,3}-?\d{2,3}-?\d{2,}|\d{3,4}-?\d{3,4})$/
+    };
+    const passwordRepeat = $('#password').val() === $('#passwordRepeat').val();
+    const city = $('#city').val() === null ? false : true;
 
-    const message = 'Uspesno ste se registrovali';
-    (async () => await postIntoDatabase('users', usersObj, message))();
-    setTimeout(() => { location.href = 'index.html'; }, 500);
+    if (input !== 'passwordRepeat') {
+        let inputValue = event.currentTarget.value;
+        if (RegEx[input].test(inputValue)) {
+            $(`.${input}`).html(`<i class="fas fa-check-circle"></i>`).css('color', 'green');
+            valid[input] = true;
+        } else {
+            $(`.${input}`).html(`<i class="fas fa-times-circle"></i>`).css('color', 'rgb(181, 68, 132)');
+            valid[input] = false;
+        }
+    } else {
+        if (passwordRepeat) {
+            $(`.${input}`).html(`<i class="fas fa-check-circle"></i>`).css('color', 'green');
+            valid[input] = true;
+        } else {
+            $(`.${input}`).html(`<i class="fas fa-times-circle"></i>`).css('color', 'rgb(181, 68, 132)');
+            valid[input] = false;
+        }
+    }
+    if (city) {
+        $(`.city`).html(`<i class="fas fa-check-circle"></i>`).css('color', 'green');
+        valid['city'] = true;
+    } else {
+        $(`.city`).html(`<i class="fas fa-times-circle"></i>`).css('color', 'rgb(181, 68, 132)');
+        valid['city'] = false;
+    }
+};
+
+function createUser() {
+    if (valid.name && valid.email && valid.password && valid.work && valid.telephone && valid.passwordRepeat && valid.city) {
+        const usersObj = {};
+        $("#writeRecipe").find("input, select, textarea").each(function () {
+            usersObj[this.name] = $(this).val();
+        });
+        delete usersObj.passwordRepeat;
+        usersObj['favorites'] = [];
+
+        const message = 'Uspesno ste se registrovali';
+        (async () => await postIntoDatabase('users', usersObj, message))();
+        setTimeout(() => { location.href = 'index.html'; }, 500);
+    } else {
+        alert('Nevalidan unos!\nMolimo Vas, popunite sva tražena polja.');
+    }
 };
 
 function createRecipe(methodPost = true) {
@@ -501,6 +545,8 @@ function onLoadPageHTML() {
         return loadRecipeToForm();
     } else if (page.search('/user_panel.html') >= 0) {
         return usersRecipes();
+    } else if (page.search('/register.html') >=0){
+        return $('#writeRecipe input').on('input', validationCheck);
     }
 };
 
@@ -511,6 +557,7 @@ function eventsAll() {
     $('#userPanel').on('click', goToUserPanel);
     $('#logIn').on('click', userLogIn);
     $('#createUser').on('click', createUser);
+    $('#resetUser').on('click', () => location.reload());
     $('#logIn-out').on('click', logInOut);
     $('#createRecipe').on('click', () => createRecipe());
     $('#searchRecipesAll, #searchRecipesAll_2').on('click', async () => await searchRecipes('.recipes-container', '.recipes-click-scroll'));
